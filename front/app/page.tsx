4726919dbd523b71ccf872b09c3fc7ca61225d2b
Anurag Srivastav <anuragsrivastav0027@gmail.com>
2024-05-08 01:54:51 +0530

    refactor: updated ui for header and banner

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,14 +1,14 @@
-import Baner from "@/components/Baner";
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
-import List from "@/components/List";
+import Banner from "@/components/Landing/Banner";
+import Footer from "@/components/Landing/Footer";
+import Header from "@/components/Landing/Header";
+import TopSheets from "@/components/Landing/TopSheets";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="flex min-h-screen bg-gradient-to-b from-blue-200 to-gray-50 via-bg-white overflow-hidden w-full flex-col items-center gap-4 sm:px-8 px-4">
       <Header />
-      <Baner />
-      <List />
+      <Banner />
+      <TopSheets />
       <Footer />
     </main>
   );
