720175f8e7c0198b4c57d5cd343408f3e5f94fae
Gyanthakur <gps.96169@gmail.com>
2024-05-08 13:08:29 +0530

    feat: Added starred model

--- back/model/solve.model.js
+++ back/model/solve.model.js
@@ -2,3 +2,30 @@
 // userId
 // sheetId
 // questionId
+import mongoose from 'mongoose';
+
+const responseSchema = new mongoose.Schema({
+    id: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    sheetId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Sheet',
+        required: true
+    },
+    questionId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Question',
+        required: true
+    }
+});
+
+// Exporting the model
+export default mongoose.model('Response', responseSchema);
