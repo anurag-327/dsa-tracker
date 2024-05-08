546dee1d26eafaad905603ff1a6dff39a75f071a
Gyanthakur <gps.96169@gmail.com>
2024-05-08 13:12:21 +0530

    feat: Added solved  model

---
+++ back/model/solve.model.js
@@ -0,0 +1,31 @@
+// id
+// userId
+// sheetId
+// questionId
+import mongoose from 'mongoose';
+
+const responseSchema = new mongoose.Schema({
+    id: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    sheetId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Sheet',
+        required: true
+    },
+    questionId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Question',
+        required: true
+    }
+});
+
+// Exporting the model
+export default mongoose.model('Response', responseSchema);
